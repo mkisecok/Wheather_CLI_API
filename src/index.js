0b1053868babf68918165de6cae68f9633da3974
Mkisecok <mahmutkisecok@gmail.com>
2022-02-03 15:19:44 +0100

    Change the url with params

--- src/index.js
+++ src/index.js
@@ -3,21 +3,27 @@ const axios=require('axios');
 
 const[node,script, ...args]=process.argv;
 
+const baseUrl='https://api.openweathermap.org/data/2.5/weather?'
+
 const units=(units='metric', symbol='C')=>{ 
 
-if(args[1] === 'f' || args[1]==='F')
-    {
-        units='standard';
-        symbol='F'
-    };
-    return {units,symbol};
-}
+    if(args[1] === 'f' || args[1]==='F')
+        {
+            units='standard';
+            symbol='F'
+        };
+        return {units,symbol};
+};
 
- axios.get(`https://api.openweathermap.org/data/2.5/weather?q=${args[0]}&appid=${process.env.API_KEY}&units=${units().units}`)
+const params={
+    q:args[0],
+    appid:process.env.API_KEY,
+    units:units().units
+};
+
+ axios.get(baseUrl,{params} )
  .then(res=>{
     
-    
-
      console.log(`It is now ${res.data.main.temp}°${units().symbol } in ${res.data.name}, ${ res.data.sys.country}\n`+
      `The current weather conditions are: ${res.data.weather[0].description}`)
      
